a793b977a5b7e020779e5255277c4a3415629628
unknown <olekkorovin@gmail.com>
2023-01-17 11:57:14 +0200

    HM 6 ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,26 @@
-export const App = () => {
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
 
+export const App = () => {
   return (
-      <p>Deploy is good =D</p>
+    <div
+      style={{
+        width: 400,
+        padding: '12px 16px',
+        borderRadius: 20,
+        backgroundColor: '#006d00',
+        color: 'white',
+        textAlign: 'center',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <ContactForm />
+
+      <h2>Contacts</h2>
+      <Filter />
+
+      <ContactList />
+    </div>
   );
 };
